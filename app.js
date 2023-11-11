410dcf6c9b6614084457367b186478d015ce43d3
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-11-10 20:38:40 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const purple = [
+    document.getElementsByClassName('.img')
+]
+
+https://images.pexels.com/photos/971360/pexels-photo-971360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/1309766/pexels-photo-1309766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/594233/pexels-photo-594233.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/1927595/pexels-photo-1927595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/1381679/pexels-photo-1381679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/3078831/pexels-photo-3078831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+https://images.pexels.com/photos/2120005/pexels-photo-2120005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+
+
+
+const green = [
+https://images.pexels.com/photos/2690764/pexels-photo-2690764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+]
